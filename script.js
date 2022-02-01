5c2d6ed6a0a30d903446b7ef20083d2c99306976
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-01 14:13:58 -0800

    add fake link add first draft text color

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+link = document.getElementById("fake-link");
+link.addEventListener("click", function () {
+  alert(
+    "Whoops, you must have hit the wrong button! Hit the envelope to offer me a job."
+  );
+});
