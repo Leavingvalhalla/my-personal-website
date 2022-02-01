d955749fe64e1141d8167e50d9a17171605614a3
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-01 14:26:32 -0800

    add image add hide functionality to fake link

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 link = document.getElementById("fake-link");
-link.addEventListener("click", function () {
+link.addEventListener("click", function (e) {
   alert(
-    "Whoops, you must have hit the wrong button! Hit the envelope to offer me a job."
+    "Whoops, you must have hit the wrong button! Click the envelope to offer me a job."
   );
+  e.target.style.visibility = "hidden";
 });
